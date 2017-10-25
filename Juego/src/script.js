2c6e6404e39aa06293434ea4ea530d5b342cd73e
Makimoki70 <32273473+Makimoki70@users.noreply.github.com>
2017-10-25 11:14:27 +0200

    Add files via upload
    Creación del objeto snake y colisión de este con la comida. Y he hecho más legible el código.

--- Juego/src/script.js
+++ Juego/src/script.js
@@ -1,10 +1,43 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, '', { preload: preload, create: create, update: update });
+var cursors;
+
 
 var cursors;
 
 var player;
 
 function preload() {
+<<<<<<< HEAD
+	game.load.image('food', 'asset/pizza.png');
+	game.load.image('snake', 'asset/circle.png');
+}
+
+function create() {
+	game.physics.startSystem(Phaser.Physics.ARCADE);
+	initFood();
+	initSnake();
+	cursors = game.input.keyboard.createCursorKeys();
+}
+
+function update() {
+	player.sprite.movimiento(player.sprite);
+	game.physics.arcade.overlap(player.sprite, food.sprite, eatFood, null, this);
+}
+
+function initFood() {
+	var rx = Math.floor((Math.random() * 800) + 1);
+	var ry = Math.floor((Math.random() * 600) + 1);
+	food = new Food(game, rx, ry);
+	return food;
+	
+}
+
+function initSnake() {
+	var rx = Math.floor((Math.random() * 800) + 1);
+	var ry = Math.floor((Math.random() * 600) + 1);
+	player = new Snake(game, rx, ry);
+	return player;
+=======
     game.load.image('food', 'asset/pizza.png');
     game.load.image('snek', 'asset/snek.jpg', 32, 48);
 }
@@ -42,9 +75,10 @@ function update() {
     {player.body.velocity.y= 50;
      player.body.velocity.x= 0;  
 }
+>>>>>>> 34208c622f6e135a96fce42019783fc5d3552408
 }
 
-function initFood(x, y) {
-        var f = new Food(game, x, y);
-        return f;
+function eatFood(player,food) {
+	food.kill();
+	initFood();
 }
