e1c92f66855c900a8ae5482517149b3a7ab3eda8
gabrielmubo <gabrielmubo@gmail.com>
2017-10-29 23:13:52 +0100

    El uso de game como función y llamada anónima en el hmtl a dicha función

--- Juego/src/script.js
+++ Juego/src/script.js
@@ -1,63 +1,83 @@
-var game = new Phaser.Game(800, 600, Phaser.CANVAS, '', { preload: preload, create: create, update: update });
+Game = function(game)
+{
+	this.score = 0;
+	this.scoreText;
 
-var cursors;
-
-var player;
-
-var score = 0;
-var scoreText;
-
-var music;
-var eatSound;
-
-function preload() {
-	game.load.image('food', 'asset/pizza.png');
-	game.load.image('snake', 'asset/circle.png');
-
-	game.scale.scaleMode = Phaser.ScaleManager.RESIZE;
-
-	game.load.audio('mainTheme', 'asset/MainTheme.mp3');
-	game.load.audio('eatSound', 'asset/EatSound.ogg');
+	this.music;
+	this.eatSound;
 }
 
-function create() {
-	game.physics.startSystem(Phaser.Physics.P2JS);
-	initFood();
+Game.prototype = {
+	preload: function(){
+		this.game.load.image('food', 'asset/pizza.png');
+		this.game.load.image('snake', 'asset/circle.png');
 
-	game.scale.setResizeCallback(function () {
-		if (food.sprite.position.x > game.width || food.sprite.position.y > game.height) {
-			food.sprite.kill();
+		this.game.scale.scaleMode = Phaser.ScaleManager.RESIZE;
+		this.game.load.audio('mainTheme', 'asset/MainTheme.mp3');
+		this.game.load.audio('eatSound', 'asset/EatSound.ogg');
+	},
+
+	create: function(){
+
+		var width = this.game.width;
+    	var height = this.game.height;
+
+		this.game.world.setBounds(-width, -height, width*2, height*2);
+    	this.game.stage.backgroundColor = '#444';
+    	 
+    	//initialize physics and groups 
+ 		this.game.physics.startSystem(Phaser.Physics.P2JS);
+
+
+		this.initFood();
+
+    	//create player
+    	var snake = new Snake(this.game,'snake', 100, 300);
+
+
+
+    	
+
+		this.game.scale.setResizeCallback(function () {
+		if (this.food.sprite.position.x > this.game.width || this.food.sprite.position.y > this.game.height) {
+			this.food.sprite.kill();
 			initFood();
 		}
-	});
+		});
 
-	var snake = new Snake(this.game,'snake', 100, 300);
-	cursors = game.input.keyboard.createCursorKeys();
 
-	music = game.add.audio('mainTheme');
-	music.loopFull();
-	eatSound = game.add.audio('eatSound');
+		this.music = this.game.add.audio('mainTheme');
+		this.music.loopFull();
+		this.eatSound = this.game.add.audio('eatSound');
 
-	scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#FF0000' });
-}
+		this.scoreText = this.game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#FF0000' });
+		},
 
-function update() {
+	update: function() {
+    //update game components
+    	for (var i = 0; i<this.game.snakes.length; i++) {
+        	this.game.snakes[i].update();
+    	}
+
+    	//this.game.physics.arcade.overlap(this.game.snake, this.game.food, this.eatFood, null, this);
+	},
 	
-}
+	initFood: function() {
+			var rx = Math.floor((Math.random() * this.game.width) + 1);
+			var ry = Math.floor((Math.random() * this.game.height) + 1);
+			var food = new Food(this.game, rx, ry);
+			return food;
+	},
 
-function initFood() {
-	var rx = Math.floor((Math.random() * 800) + 1);
-	var ry = Math.floor((Math.random() * 600) + 1);
-	food = new Food(game, rx, ry);
-	return food;	
-}
-
-
-function eatFood(player, food) {
+	
+ /*
+	function eatFood(player, food) {
 	eatSound.play();
 	food.kill();
 	initFood();
 
 	 score += 10;
     scoreText.text = 'Score: ' + score;
-}
+}  
+*/
+};
