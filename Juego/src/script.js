00dc15315db8edbe7c04314828eb1650c6b224f7
elshuck <jltrwork@gmail.com>
2017-10-29 12:12:33 +0100

    Correción de la mezcla de subida

--- Juego/src/script.js
+++ Juego/src/script.js
@@ -1,13 +1,9 @@
 var game = new Phaser.Game(800, 600, Phaser.CANVAS, '', { preload: preload, create: create, update: update });
 var cursors;
 
-
-var cursors;
-
 var player;
 
 function preload() {
-<<<<<<< HEAD
 	game.load.image('food', 'asset/pizza.png');
 	game.load.image('snake', 'asset/circle.png');
 }
@@ -37,46 +33,8 @@ function initSnake() {
 	var ry = Math.floor((Math.random() * 600) + 1);
 	player = new Snake(game, rx, ry);
 	return player;
-=======
-    game.load.image('food', 'asset/pizza.png');
-    game.load.image('snek', 'asset/snek.jpg', 32, 48);
 }
 
-function create() {
-
-player = game.add.sprite(32, 150, 'snek');
-player.scale.setTo(0.2, 0.2);
-game.physics.arcade.enable(player);
-player.body.collideWorldBounds = true;
-player.body.velocity.x=50;
-cursors = game.input.keyboard.createCursorKeys();
-}
-
-function update() {
-    initFood(100,100);
-
-
-
-//movimientos de la serpiente
-     if(cursors.left.isDown && player.body.velocity.x !=50)
-    {player.body.velocity.x =-50;
-     player.body.velocity.y= 0;   
-        
-    }
-    if(cursors.right.isDown && player.body.velocity.x !=-50)
-    {player.body.velocity.x= 50;
-    player.body.velocity.y= 0;
-    }
-    if(cursors.up.isDown && player.body.velocity.y !=50)
-    {player.body.velocity.y= -50;
-    player.body.velocity.x= 0;
-    }
-    if(cursors.down.isDown && player.body.velocity.y !=-50)
-    {player.body.velocity.y= 50;
-     player.body.velocity.x= 0;  
-}
->>>>>>> 34208c622f6e135a96fce42019783fc5d3552408
-}
 
 function eatFood(player,food) {
 	food.kill();
